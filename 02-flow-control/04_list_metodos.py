8a1347ae9737b767173b1e71b0bf94de9afaf43c
Airy-Dev6 <airysilva11@gmail.com>
2025-02-26 22:17:14 -0600

    add 04

---
+++ 02-flow-control/04_list_metodos.py
@@ -0,0 +1,27 @@
+
+
+import os
+
+os.system('clear')
+
+###
+# 04 - Listas metodos 
+# Los metodos importantes para trabajar con listas  
+###
+
+lista1 = [1,2,3,4,5,6,7]
+
+lista1.append(8) # Agrega un elemento al final 
+
+print(lista1)
+
+lista2 = ['a', 'b', 'c', 'd'] 
+lista2.append('e')
+print(lista2)
+
+lista2.insert(1, '@')
+
+print(lista2)
+
+
+
